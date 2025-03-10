5296029237fdc1daa56841c44a6e1e1a102167de
Nyck-i <nycolle.huida@escola.pr.gov.br>
2025-03-10 09:19:42 -0300

    Botão diminuir fonte

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
     let tamanhoAtualfonte = 1;
     aumentaFonteBotao.addEventListener('click',function(){
         tamanhoAtualfonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualfonte}`
+        document.body.style.fontSize = `${tamanhoAtualfonte}rem`;
+
     });
-}); 
+
+diminuiFonteBotao.addEventListener('click',function(){
+    tamanhoAtualfonte -= 0.1;
+    document.body.style.fontSize = `${tamanhoAtualfonte}rem`;
+
+});
+});
