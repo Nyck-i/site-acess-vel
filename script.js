049331952f637953dd09867c1e865e5f9c622abd
Nyck-i <nycolle.huida@escola.pr.gov.br>
2025-04-07 10:56:32 -0300

    alto-contraste

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-    const alternaContraste= document.getElementById('alterna-contraste')
+    const alternaContraste= document.getElementById('alterna-contraste');
     
     let tamanhoAtualFonte = 1;
 
@@ -25,5 +25,8 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
+    })
+    alterrnaContraste.addEventListener('click',function(){
+        document.body.classList.toggle('alto-contraste')
     })
 })
